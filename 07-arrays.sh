f65e6929d0a8c0895859dc3f9d9c96de1101d1f6
saidevops32 <devopsengsai@gmail.com>
2024-01-27 04:04:29 +0530

    2nd cmmit

---
+++ 07-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fruits=("apple" "banana" "carrot")
+
+echo "printing first fruit in array : $fruits[0] "
+
+echo "printing second fruit in array : $fruits[1] "
+
+echo "printing third fruit in array : $fruits[2] "
+
+echo "printing all fruits in array : $fruits[@] "
