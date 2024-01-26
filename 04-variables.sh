60102f169ecb826d89f9bde8fdb6aff07560009f
saidevops32 <devopsengsai@gmail.com>
2024-01-27 01:41:18 +0530

    2nd cmmit

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 : hai $person2 good morning"
+echo "$person2 : hai$person1 very good morning"
+echo "$person1 : how are you  $person2"
+echo "$person2 : i am doing good how are you  $person1"
