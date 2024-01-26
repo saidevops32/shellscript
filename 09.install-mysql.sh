591fbca652f471f0ee3a756ea23acea68053ee51
saidevops32 <devopsengsai@gmail.com>
2024-01-27 05:02:26 +0530

    2nd cmmit

---
+++ 09.install-mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne o]
+then
+  echo "error: you are not user become root user"
+  exit 1
+else
+  echo " you are a root user"   
+fi
+
+yum install mysql -y
+
+if [$? -ne 0]
+then
+  echo "instaling mysql is failed"
+  exit 1
+else
+  echo "inatalling mysql is success "
