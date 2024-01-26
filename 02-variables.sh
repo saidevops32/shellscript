67211df0606eaa8cbc845d771db7bec2ef0b4e65
saidevops32 <devopsengsai@gmail.com>
2024-01-26 10:17:55 +0530

    changes for folders

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!?bin/bash
+
+echo "ramesh : hai suresh good morning"
+echo "suresh : hai ramesh very good morning"
+echo "ramesh : how are you suresh"
+echo "suresh : i am doing good how are you suresh"
