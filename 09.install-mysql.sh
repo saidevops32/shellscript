aca712a8cc875b8bd7076ec6159f160bd9b48384
saidevops32 <devopsengsai@gmail.com>
2024-01-27 05:08:14 +0530

    2nd cmmit

--- 09.install-mysql.sh
+++ 09.install-mysql.sh
@@ -4,7 +4,7 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-  echo "error: you are not root user become root user"
+  echo "error: you are not a root user become root user"
   exit 1
 else
   echo " you are a root user"   
@@ -18,3 +18,4 @@ then
   exit 1
 else
   echo "inatalling mysql is success "
+fi  
