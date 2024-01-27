e149ff4981b33412abbf18a026b416049c3aabf8
saidevops32 <devopsengsai@gmail.com>
2024-01-27 23:50:32 +0530

    shell script

--- 13-installin-packages.sh
+++ 13-installin-packages.sh
@@ -11,9 +11,9 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VAIDATE(){
     if [ $1 -ne 0 ]
     then
-      echo -e "$2 is ..$R failed $N"
+         echo -e "$2 is ..$R failed $N"
     else
-      echo -e "$2 iS.. $G success $N"  
+         echo -e "$2 iS.. $G success $N"  
     fi  
 }
 
@@ -21,10 +21,10 @@ echo "script started excuting $TIMESATMP"
 
 if [ $ID -ne 0 ]
 then
-  echo -e "error : you are not a root user $R become root user $N"
-  exit 1
+     echo -e "error : you are not a root user $R become root user $N"
+     exit 1
 else
-  echo "you are a root user"
+     echo "you are a root user"
 fi  
 
 for package in $@
@@ -32,9 +32,9 @@ do
   yum list installed $package
   if [ $? -ne 0 ]
   then 
-    yum install $package &>> $LOGFILE
-    VALIDATE $? installing $package
+       yum install $package &>> $LOGFILE
+       VALIDATE $? installing $package
   else
-    echo "$package already installed ..$Y skipping $N"  
+       echo "$package already installed ..$Y skipping $N"  
   fi  
 done
