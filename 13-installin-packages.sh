64225d134488630d9787aec6489697b566083393
saidevops32 <devopsengsai@gmail.com>
2024-01-27 23:26:02 +0530

    shell script

---
+++ 13-installin-packages.sh
@@ -0,0 +1,39 @@
+ID=$(id -u)
+
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VAIDATE (){
+    if [$1 -ne 0]
+    then
+      echo "$2 is ..$R failed $N"
+    else
+      echo "$2 iS.. $G success $N"  
+}
+
+echo "script started excuting $TIMESATMP"
+
+if [$ID -ne 0]
+then
+  echo "error : you are not a root user $R become root user $N"
+  exit 1
+else
+  echo "you are a root user"
+fi  
+
+for package in $@
+do
+  yum installed $package
+  if [$? -ne 0 ]
+  then 
+    yum install $package &>> $LOGFILE
+    VALIDATE $? installing $package
+  else
+    echo "$package already installed ..$Y skipping $N"  
+  fi  
+done
