b6b4c2c37dfbbc24005400fece2a2866f92acbce
saidevops32 <devopsengsai@gmail.com>
2024-01-27 06:15:23 +0530

    2nd cmmit

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
    if [ $1 -ne 0 ] 
@@ -20,10 +23,10 @@ else
   echo "you are a root user"   
 fi
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? MYSQL
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 VALIDATE $? GIT
