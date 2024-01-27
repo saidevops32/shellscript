889b0962f201915276f4adf80ecca214059e8cc5
saidevops32 <devopsengsai@gmail.com>
2024-01-27 06:31:35 +0530

    2nd cmmit

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,21 +3,25 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
    if [ $1 -ne 0 ] 
    then
-       echo "instaling $2 is failed"
+       echo "instaling $2 is $R failed $N"
        exit 1
    else
-       echo "inatalling $2 is success "
+       echo "installing $2 is $R success $N "
    fi  
 }
    
 if [ $ID -ne 0 ]
 then
-  echo "error: you are not a root user become root user"
+  echo " $R error: you are not a root user become root user $N"
   exit 1
 else
   echo "you are a root user"   
